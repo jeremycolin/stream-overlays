7330903320fa5cea088ae4996ee5c5b086e0256d
Jeremy Colin <jeremypa.colin@gmail.com>
2022-02-12 13:53:07 +0100

    use heroku port config

--- packages/server/src/config.ts
+++ packages/server/src/config.ts
@@ -1,6 +1,6 @@
 export const isDev = process.env.NODE_ENV !== "production";
 
 export const config = {
-  SERVER_PORT: isDev ? 8080 : 443,
+  SERVER_PORT: isDev ? 8080 : process.env.PORT,
   SOCKET_SERVER_PORT: 3000,
 };
--- packages/server/src/health.ts
+++ packages/server/src/health.ts
@@ -1,13 +1,17 @@
-const TWITCH_API_TOKEN = process.env.TWITCH_API_TOKEN;
-const TWITCH_CLIENT_ID = process.env.TWITCH_CLIENT_ID;
-const TWITCH_CLIENT_SECRET = process.env.TWITCH_CLIENT_SECRET;
+import { isDev } from "./config";
 
-if (!TWITCH_API_TOKEN) {
+if (!process.env.TWITCH_API_TOKEN) {
   throw Error("ENV variable TWITCH_API_TOKEN missing");
 }
-if (!TWITCH_CLIENT_ID) {
+if (!process.env.TWITCH_CLIENT_ID) {
   throw Error("ENV variable TWITCH_CLIENT_ID missing");
 }
-if (!TWITCH_CLIENT_SECRET) {
+if (!process.env.TWITCH_CLIENT_SECRET) {
   throw Error("ENV variable TWITCH_CLIENT_SECRET missing");
+}
+
+if (!isDev) {
+  if (!process.env.PORT) {
+    throw Error("ENV variable PORT missing");
+  }
 }
