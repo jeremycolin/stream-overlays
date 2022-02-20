b3155a30845628993d02e733f877d0de8d4c230e
Jeremy Colin <jeremypa.colin@gmail.com>
2022-02-20 20:23:09 +0100

    Update samyz_ live to fillou for now

---
+++ packages/client/src/scenes/samyz_/live.vue
@@ -0,0 +1,26 @@
+<script>
+import followFilou from "@/alerts/followFilou.vue";
+
+export default {
+  name: "liveScene",
+  components: {
+    followFilou,
+  },
+  mounted() {
+    console.log("Scene -> Samyz live");
+  },
+};
+</script>
+
+<template>
+  <div class="live-scene">
+    <followFilou></followFilou>
+  </div>
+</template>
+
+<style lang="scss">
+.live-scene {
+  width: 100%;
+  height: 100%;
+}
+</style>
