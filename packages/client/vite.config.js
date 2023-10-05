a9e4d016dbc9d8589a8d4a1b0d1b96ceebd43ecc
lbineau <lory.bineau@gmail.com>
2023-10-05 20:44:48 +0200

    Refactor dotenv to be shared between client and server (#13)
    * refactor dotenv to be shared between client and server
    
    * add twitch url to env mode .env to root
    
    ---------
    
    Co-authored-by: Jeremy Colin <jeremypa.colin@gmail.com>

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -1,3 +1,7 @@
+import dotenv from "dotenv";
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
@@ -10,10 +14,8 @@ export default defineConfig({
   },
   define: {
     IS_DEV: process.env.NODE_ENV !== "production",
-    APP_CLIENT_ID: JSON.stringify("fvetqbhasvewapnb7glpbqw8ic4im0"),
-    TWITCH_OAUTH_REDIRECT_URL: JSON.stringify(
-      process.env.NODE_ENV === "production" ? "https://stream-overlays-production.up.railway.app/oauth" : "http://localhost:5000/oauth"
-    ),
+    APP_CLIENT_ID: JSON.stringify(process.env.TWITCH_CLIENT_ID),
+    TWITCH_OAUTH_REDIRECT_URL: JSON.stringify(process.env.TWITCH_OAUTH_REDIRECT_URL),
     __WEBSOCKET_ENDPOINT__: JSON.stringify(
       process.env.NODE_ENV === "production" ? "wss://stream-overlays-production.up.railway.app/" : "ws://localhost:3000/"
     ),
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,4 +1,6 @@
-import "dotenv/config";
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, "../../../.env") });
 import express from "express";
 import { createServer } from "http";
 import cors from "cors";
