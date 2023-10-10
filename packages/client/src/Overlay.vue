e04aa1e5aca924d1e257d3f49d71f984d0d29219
Jérémy <jeremzor@gmail.com>
2023-10-10 19:01:47 +0200

    UI on auth page (#14)

--- packages/client/src/Overlay.vue
+++ packages/client/src/Overlay.vue
@@ -2,6 +2,7 @@
 import { computed } from "vue";
 import { io } from "socket.io-client";
 import { EventTypes, OauthTypes, TWITCH_GAMES } from "api";
+import AppTitle from "@/components/title.vue";
 import SceneManager from "@/scenes/manager.vue";
 import FollowManager from "@/alerts/follow/manager.vue";
 import { nonce } from "@/lib/auth";
@@ -9,6 +10,7 @@ import { nonce } from "@/lib/auth";
 export default {
   name: "overlay",
   components: {
+    AppTitle,
     SceneManager,
     FollowManager,
   },
@@ -73,14 +75,20 @@ export default {
 
 <template>
   <div v-if="state === 'loading'" class="auth">
-    <div class="text-info">Loading..</div>
+    <app-title></app-title>
+    <div class="card">
+      <div class="text-info">Loading..</div>
+    </div>
   </div>
   <div v-else-if="state === 'oauth:start'" class="auth">
-    <div class="text-info">
-      <div>Hey {{ user }}!</div>
-      <div class="details">Looks like we need to renew your Twitch permissions</div>
+    <app-title></app-title>
+    <div class="card">
+      <div class="text-info">
+        <h3>Hey {{ user }}! 👋</h3>
+        <p class="details">Looks like we need to renew your Twitch permissions</p>
+      </div>
+      <button @click="handleAuth" class="auth-button">{{ "Allow" }}</button>
     </div>
-    <button @click="handleAuth" class="auth-button">{{ "Allow Twitch permissions for overlays" }}</button>
   </div>
   <div v-else class="stream-overlay" :class="{ 'is-dev': isDev }">
     <SceneManager />
@@ -90,38 +98,57 @@ export default {
 
 <style lang="scss">
 .auth {
+  position: relative;
   width: 100%;
   height: 100%;
-  background-color: lightcoral;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  background-image: radial-gradient(farthest-corner circle at 100% 0%, #00dfff 0%, #8c00ea 100%);
+
+  .card {
+    width: 560px;
+    padding: 1.5rem;
+    color: #04060a;
+    backdrop-filter: blur(16px) saturate(180%);
+    -webkit-backdrop-filter: blur(16px) saturate(180%);
+    background-color: rgba(255, 255, 255, 0.75);
+    border-radius: 12px;
+    border: 1px solid rgba(209, 213, 219, 0.3);
+  }
 
   .text-info {
-    color: white;
-    font-size: 4rem;
-    position: relative;
-    top: -10%;
-
+    h3 {
+      font-weight: 500;
+      margin: 0;
+      text-transform: capitalize;
+    }
     .details {
       margin-top: 1rem;
-      font-size: 2rem;
+      color: rgb(75, 86, 99);
     }
   }
 
   .auth-button {
-    position: relative;
-    top: -10%;
-
-    margin-top: 4rem;
-    font-size: 2rem;
-    padding: 1rem 2rem;
-    color: darkslategray;
-    border: 1px solid white;
-    border-radius: 20px;
     cursor: pointer;
-    display: inline-block;
+    background-color: rgb(75, 86, 99);
+    background-image: linear-gradient(rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0));
+    border: 1px solid #6b7280;
+    color: white;
+    border-radius: 0.375rem;
+    font-size: 0.875rem;
+    padding: 0.375rem 0.75rem;
+    box-shadow: 0 1px 1px rgba(18, 21, 26, 0.075);
+    font-weight: 500;
+    line-height: 1.5rem;
+    padding: 0.5rem 1rem;
+    text-align: center;
+    user-select: none;
+
+    &:hover {
+      background-color: #404954;
+    }
   }
 }
 
