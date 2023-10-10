4c22ae34314c05e15a3bb2ff5acace42d3d7b6d4
Jérémy Brialon <jeremzor@gmail.com>
2023-10-10 17:15:48 +0200

    UI on auth page

---
+++ packages/client/src/components/title.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  name: "AppTitle",
+};
+</script>
+
+<template>
+  <h1 class="app-title">
+    <span class="visually-hidden">Stream Overlay</span>
+    <svg aria-hidden="true" id="lockup-title-mask" class="app-title lockup-title-mask visually-hidden">
+      <clipPath id="lockup-title-mask-path">
+        <text dominant-baseline="hanging" text-anchor="middle" x="50%" y="0em" dy="0.125em">Stream Overlay</text>
+      </clipPath>
+    </svg>
+  </h1>
+</template>
+
+<style lang="scss">
+h1 {
+  position: absolute;
+  color: white;
+  top: 0;
+  left: 0;
+}
+
+.app-title {
+  width: 280px;
+  height: 50px;
+}
+@supports (backdrop-filter: blur(12px)) {
+  .visually-hidden {
+    position: absolute;
+    top: -9999px;
+    left: -9999px;
+  }
+  h1 {
+    backdrop-filter: blur(12px);
+    background: rgba(white, 0.2);
+    clip-path: url(#lockup-title-mask-path);
+  }
+}
+</style>
