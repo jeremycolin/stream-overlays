cf5dda747c947b2965cb86c0be9fa4dd6d5152a6
lbineau <lory.bineau@gmail.com>
2023-10-16 01:10:02 +0200

    lbineau: add start view with binding of isaac theme

--- packages/client/src/scenes/lbineau/start.vue
+++ packages/client/src/scenes/lbineau/start.vue
@@ -1,22 +1,28 @@
 <script>
 import * as PIXI from "pixi.js";
-import { CRTFilter, RGBSplitFilter, GlitchFilter } from "pixi-filters";
-import backgroundUrl from "@/assets/background-code.png";
+import { CRTFilter, RGBSplitFilter, GlitchFilter, DropShadowFilter, ColorOverlayFilter, OutlineFilter } from "pixi-filters";
+import backgroundUrl from "@/assets/isaac/isaac.jpg";
+import FontFaceObserver from 'fontfaceobserver';
 
 let app;
 let stage;
 let renderer;
 let ticker;
 let texture;
+let loadingImageBackground;
 let background;
 let filterRGB;
 let filterCRT;
 let filterTextRGB;
 let filterGlitch;
+let filterColor;
+let filterOuline;
 let text;
+let intervalSprite;
+let intervalSpriteVariant;
 export default {
   name: "startScene",
-  mounted() {
+  async mounted() {
     console.log("Scene -> Default Start");
     app = new PIXI.Application({
       width: 1920,
@@ -34,8 +40,18 @@ export default {
     background = new PIXI.Sprite(texture);
 
     background.anchor.set(0.5);
-    background.x = 1920 / 2;
-    background.y = 1080 / 2;
+    background.x = app.screen.width / 2;
+    background.y = app.screen.height / 2;
+
+    loadingImageBackground = new PIXI.Sprite();
+    loadingImageBackground.anchor.set(0.5);
+    loadingImageBackground.x = app.screen.width / 2;
+    loadingImageBackground.y = app.screen.height / 2 + 250;
+    loadingImageBackground.scale.set(3);
+    filterColor = new ColorOverlayFilter(0xFFFFFF);
+    // applying big outline filter creates these drawing-like lines
+    filterOuline = new OutlineFilter(40, 0x000000);
+    loadingImageBackground.filters = [filterColor, filterOuline];
 
     // create filters from pixi filters
     filterRGB = new RGBSplitFilter();
@@ -50,7 +66,10 @@ export default {
     filterCRT.time = 0.5;
 
     background.filters = [filterCRT, filterRGB];
+
     stage.addChild(background);
+
+    stage.addChild(loadingImageBackground);
 
     filterTextRGB = new RGBSplitFilter();
     filterTextRGB.red = [-2, 0];
@@ -58,19 +77,30 @@ export default {
     filterTextRGB.blue = [0, 0];
     filterGlitch = new GlitchFilter();
 
+    let font = new FontFaceObserver('Isaac', {});
+    try {
+      await font.load();      
+    } catch (error) {
+      console.warn(error);
+    }
+
     // TEXT
-    text = new PIXI.Text("STREAM OVERLAY", {
-      fontFamily: "Arial",
+    text = new PIXI.Text("J'arrive\nbientôt !", {
+      fontFamily: "Isaac",
       fontSize: 200,
+      lineHeight: 150,
       fill: 0xffffff,
+      strokeThickness: 40,
+      lineJoin: 'round',
+      align: 'center'
     });
 
     text.anchor.set(0.5);
-    text.x = 1920 / 2;
-    text.y = 1080 / 2;
+    text.x = app.screen.width / 2;
+    text.y = app.screen.height / 2 - 200;
 
     text.filters = [filterGlitch, filterTextRGB];
-
+    
     ticker = PIXI.Ticker.shared;
     ticker.add(() => {
       filterCRT.seed = Math.random();
@@ -83,6 +113,26 @@ export default {
       filterTextRGB.red = [0, Math.random() * 4];
       renderer.render(stage);
     });
+
+    // handle sprite changes
+    let spriteIdx = 1;
+    const spriteIdxMax = 56;
+    let spriteVariantIdx = 0;
+    intervalSprite = setInterval(() => {
+      if (spriteIdx < spriteIdxMax) {
+        spriteIdx++
+      }
+      spriteIdx = (spriteIdx < spriteIdxMax) ? spriteIdx + 1 : 1;
+    }, 4000);
+
+    intervalSpriteVariant = setInterval(() => {
+      const texture1 = PIXI.Texture.from(`/isaac/loading/loadimages-${spriteIdx.toString().padStart(3, '0')}.png`);
+      const texture2 = PIXI.Texture.from(`/isaac/loading/loadimages-${spriteIdx.toString().padStart(3, '0')}_2.png`);
+      loadingImageBackground.texture = spriteVariantIdx % 2 === 0 ? texture1 : texture2;
+      spriteVariantIdx++;
+    }, 200);
+    // EoF handle sprite changes
+
     stage.addChild(text);
   },
   unmounted() {
@@ -96,6 +146,11 @@ export default {
     filterCRT = null;
     filterTextRGB = null;
     text = null;
+    loadingImageBackground = null;
+    filterColor = null;
+    filterOuline = null;
+    clearInterval(intervalSprite);
+    clearInterval(intervalSpriteVariant);
   },
 };
 </script>
@@ -104,11 +159,14 @@ export default {
 </template>
 
 <style lang="scss">
+@import "./fonts.scss";
+
 .waiting-screen {
   display: flex;
   position: relative;
   width: 1920px;
   height: 1080px;
+  background-color: white;
 
   // Lory je sais que je devrais faire ça en css grid
   canvas {
