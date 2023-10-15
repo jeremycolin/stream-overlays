aac5cb637dfb25ee282320e3817086c704d9e493
lbineau <lory.bineau@gmail.com>
2023-10-15 21:44:19 +0200

    copy default to lbineau folder

---
+++ packages/client/src/scenes/lbineau/end.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: "endScene",
+  mounted() {
+    console.log("Scene -> Default End");
+  },
+};
+</script>
+
+<template>
+  <div class="end-scene"></div>
+</template>
+
+<style lang="scss">
+.end-scene {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ packages/client/src/scenes/lbineau/live.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: "liveScene",
+  mounted() {
+    console.log("Scene -> Default Live");
+  },
+};
+</script>
+
+<template>
+  <div class="live-scene"></div>
+</template>
+
+<style lang="scss">
+.live-scene {
+  width: 100%;
+  height: 100%;
+}
+</style>
