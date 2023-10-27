c7536c23fad1a186d7dcf8e0acee3f6c5285b7bd
lbineau <lory.bineau@gmail.com>
2023-10-28 01:49:16 +0200

    hearthstone: center text

--- packages/client/src/alerts/follow/games/hearthstone.vue
+++ packages/client/src/alerts/follow/games/hearthstone.vue
@@ -44,6 +44,24 @@ let events;
 export default {
   name: "alert-follow-hearthstone",
   inject: ["followEvent"],
+  data () {
+    return {
+      username: '',
+      // we assume 17 characters are able to fit, this part could be reworked with the computation of real length
+      // because the number of characters that can actually fit depends on characters withs
+      usernameMaxLength: 17
+    }
+  },
+  computed: {
+    // limit number of characters for the username
+    clampedUsername () {
+      return this.username.substring(this.username, this.usernameMaxLength);
+    },
+    // offset the username so it appears centered
+    startOffset () {
+      return `${(1 - this.clampedUsername.length / this.usernameMaxLength) * 50}%`
+    }
+  },
   watch: {
     followEvent(newVal) {
       if (newVal) {
@@ -158,7 +176,7 @@ export default {
     */
     /* TODO EoF remove */
 
-    this.$refs.username.innerHTML = "testFromUser";
+    this.username = "testFromUser";
 
     await addToPixiLoader(app, "FranklinMedium", "/fonts/FranklinMedium.otf");
     titleText = new PIXI.Text("Râle d'agonie:", {
@@ -241,16 +259,25 @@ export default {
     <div class="follow-hearthstone__notification" ref="notification">
       <div class="follow-hearthstone__container">
         <div class="follow-hearthstone__canvas" ref="canvas"></div>
-        <svg class="svg" viewBox="0 0 282 60">
+        <svg class="svg" viewBox="0 0 300 60">
+          <defs>
+            <!-- defines the gradient for the fade mask -->
+            <linearGradient id="svgFadeGradient">
+              <stop stop-color="black" offset="0%" />
+              <stop stop-color="white" offset="3%" />
+              <stop stop-color="white" offset="97%" />
+              <stop stop-color="black" offset="100%" />
+            </linearGradient>
+          </defs>
           <!-- 1. curve: define the bezier curve the Text will follow -->
-          <path id="svgCurve" fill="none" stroke="none" d="m0,22.12s12.48,7.37,67-5C111.1,7.11,174.62-1.11,213,.12c43.98,1.42,52,7,68,14" transform="translate(0 20)" />
+          <path id="svgCurve" fill="none" stroke="none" d="m0,22.12s12.48,7.37,67-5C111.1,7.11,174.62-1.11,213,.12c43.98,1.42,52,7,68,14" transform="translate(10 25)" />
           <!--
             2. text: define and display the Text
             - textPath will make it follow the curve
           -->
-          <text id="svgText" x="0" y="0">
+          <text id="svgText" x="0" y="0" mask="url(#fade)">
             <!-- 2.1. textPath: follow the curve -->
-            <textPath xlink:href="#svgCurve" ref="username"></textPath>
+            <textPath xlink:href="#svgCurve" :startOffset="startOffset" v-text="clampedUsername"></textPath>
           </text>
           <!--
             3. Stroke: it is not possible to add "outer" stroke so we need to add a trick to achieve it
@@ -262,8 +289,12 @@ export default {
             stroke-width="6" stroke="black" fill="none"
             mask="url(#svgTextStrokeOnly)"/>
           <mask id="svgTextStrokeOnly">
-            <rect x="0" y="0" width="282" height="60" fill="white"/>
+            <rect x="0" y="0" width="300" height="60" fill="white"/>
             <use xlink:href="#svgText" fill="black"/>
+          </mask>
+          <!-- 4. Mask: mask the text outside of the SVG with a slight fade -->
+          <mask maskUnits="userSpaceOnUse" id="fade">
+            <rect fill="url(#svgFadeGradient)" width="100%" height="100%" />
           </mask>
         </svg>
       </div>
@@ -302,9 +333,9 @@ export default {
     // background-color: tomato;
     position: absolute;
     z-index: 11;
-    top: 295px;
-    left: 60px;
-    width: 282px;
+    top: 290px;
+    left: 55px;
+    width: 300px;
     height: 60px;
     font-family: 'Belwe';
     fill: #ffffff;
