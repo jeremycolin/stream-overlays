b2e35f5e25f73dba41ee3e5b57aaef53d0d0d572
Jérémy <jeremzor@gmail.com>
2023-10-28 17:59:11 +0200

    add 127.0.0.1 to cors (#29)
    for debugging purpose on OBS mac

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -68,7 +68,7 @@ async function startServer() {
     isDev
       ? {
           cors: {
-            origin: "http://localhost:5000",
+            origin: ["http://localhost:5000", "http://127.0.0.1:5000"],
           },
         }
       : {}
