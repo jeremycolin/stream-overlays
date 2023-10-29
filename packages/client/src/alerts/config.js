1e0d64c55144d8c966136ccc145d25ea79c456b2
Jeremy Colin <jeremypa.colin@gmail.com>
2023-10-29 12:30:54 +0100

    Create parametrizable alerts config (#31)
    Co-authored-by: Jeremy Colin <jeremy.colin@zalando.de>

---
+++ packages/client/src/alerts/config.js
@@ -0,0 +1,11 @@
+export const TWITCH_API_STREAM_GAME = "TWITCH_API_STREAM_GAME";
+
+export const alertsConfig = {
+  samyz_: {
+    follow: "sot",
+  },
+  default: {
+    follow: TWITCH_API_STREAM_GAME,
+    subscribe: TWITCH_API_STREAM_GAME,
+  },
+};
